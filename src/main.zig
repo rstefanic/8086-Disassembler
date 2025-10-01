c601b0665528498dae698ff2a3d259914bdb82b3
Robert Stefanic <rstefanic72@gmail.com>
2025-10-01 09:42:17 -0400

    Add a test to see if the disassembled output is valid
    If our disassembled output is correct, then recompiling it should
    produce the same binary as what we disassembled. This test compiles the
    test file using nasm, runs the disassembler on the compiled assembly
    file, and then compiles the disassembled result to check if the final
    result is identical byte-for-byte.

--- src/main.zig
+++ src/main.zig
@@ -160,3 +160,62 @@ pub fn main() !void {
     try code.disassemble(stdout);
     try stdout.flush();
 }
+
+test "Match disassembled version with compiled version" {
+    var test_directory = try std.fs.cwd().openDir("tests", .{ .iterate = true });
+    defer test_directory.close();
+
+    try test_directory.setAsCwd();
+    var files_iterator = test_directory.iterate();
+
+    // Compile each file of valid 8086 using nasm, disassemble the compiled
+    // code, and then re-compile the disassembled code. The final result
+    // should be the same byte-for-byte which will be checked using cmp.
+    while (try files_iterator.next()) |file| {
+        // By default, nasm will just strip the `.asm` suffix for the final bin.
+        const expected_bin_filename = expected_bin_filename: {
+            var split = std.mem.splitScalar(u8, file.name, '.');
+            break :expected_bin_filename split.next().?;
+        };
+
+        // Compile the test file to get the expected binary output.
+        const nasm_cmd = [_][]const u8{ "nasm", file.name };
+        var nasm = std.process.Child.init(&nasm_cmd, std.testing.allocator);
+        try nasm.spawn();
+        const nasm_results = try nasm.wait();
+        try std.testing.expect(nasm_results.Exited == 0);
+        defer test_directory.deleteFile(expected_bin_filename) catch @panic("Could not delete expected binary file");
+
+        // Read the binary output from the input file.
+        const expected_bin = try std.fs.cwd().readFileAlloc(std.testing.allocator, expected_bin_filename, std.math.maxInt(usize));
+        defer std.testing.allocator.free(expected_bin);
+
+        // Create a test file where we disassemble the binary for testing.
+        const test_filename = "test.asm";
+        const test_file = try test_directory.createFile(test_filename, .{});
+        defer test_directory.deleteFile(test_filename) catch @panic("Could not delete test file");
+
+        // Call disassemble and write the results to the test file.
+        var code = Code{ .data = expected_bin };
+        var buf: [1024]u8 = undefined;
+        var writer = std.io.Writer.fixed(&buf);
+        try code.disassemble(&writer);
+        try test_file.writeAll(&buf);
+
+        // Recompile the test file using nasm.
+        const test_result_filename = "test";
+        const nasm2_cmd = [_][]const u8{ "nasm", "-o", test_result_filename, test_filename };
+        var nasm2 = std.process.Child.init(&nasm2_cmd, std.testing.allocator);
+        try nasm2.spawn();
+        const nasm2_results = try nasm2.wait();
+        try std.testing.expect(nasm2_results.Exited == 0);
+        defer test_directory.deleteFile(test_result_filename) catch @panic("Could not delete test results");
+
+        // The results of recompiling the disassembled output should be byte-for-byte identical.
+        const cmp_cmd = [_][]const u8{ "cmp", expected_bin_filename, test_result_filename };
+        var cmp = std.process.Child.init(&cmp_cmd, std.testing.allocator);
+        try cmp.spawn();
+        const cmp_results = try cmp.wait();
+        try std.testing.expect(cmp_results.Exited == 0);
+    }
+}
