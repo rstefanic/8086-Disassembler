03a96dc2c32d4955a5964d20d928c913456b72b4
Robert Stefanic <rstefanic72@gmail.com>
2025-11-19 10:02:12 -0500

    Emit: Simplify `getNextByte` and use it in `parseMov`
    The old version of `getNextByte` had a memory leak when an error
    occurred. It was the whole `byte.type != expected` check. I'm confident
    there's an unexpected byte which causes an error, which in turn means
    that the LL clean up doesn't occur causing a memory leak.

--- src/emit.zig
+++ src/emit.zig
@@ -55,22 +55,10 @@ pub fn emit(self: *const Disassemble, stdout: *std.Io.Writer) !void {
     }
 }
 
-fn nextByte(node: **DoublyLinkedList.Node, expected: Disassemble.ByteType) !*Disassemble.Byte {
-    if (node.*.next) |next| {
-        const byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-        if (byte.type != expected) {
-            return switch (expected) {
-                Disassemble.ByteType.Instruction => error.ExpectedInstruction,
-                Disassemble.ByteType.ModRegRm => error.ExpectedModRegRm,
-                Disassemble.ByteType.DispLo => error.ExpectedDispLo,
-                Disassemble.ByteType.DispHi => error.ExpectedDispHi,
-                Disassemble.ByteType.DataLo => error.ExpectedDataLo,
-                Disassemble.ByteType.DataHi => error.ExpectedDataHi,
-                Disassemble.ByteType.AddrLo => error.ExpectedAddrLo,
-                Disassemble.ByteType.AddrHi => error.ExpectedAddrHi,
-            };
-        }
-        node.* = next;
+fn getNextByte(node: **DoublyLinkedList.Node) !*Disassemble.Byte {
+    if (node.*.next) |*next| {
+        const byte: *Disassemble.Byte = @fieldParentPtr("node", next.*);
+        node.* = next.*;
         return byte;
     } else {
         return error.NoNodesLeft;
@@ -85,7 +73,7 @@ fn parseMov(mov: Instructions.Mov, node: *DoublyLinkedList.Node, stdout: *std.Io
             const d_flag = m.d;
             const w_flag = m.w;
 
-            const mod_reg_rm_byte: *Disassemble.Byte = try nextByte(&current, .ModRegRm);
+            const mod_reg_rm_byte = try getNextByte(&current);
             const mod_reg_rm: Binary.ModeRegRm = @bitCast(mod_reg_rm_byte.data);
             count += 1;
 
@@ -102,29 +90,16 @@ fn parseMov(mov: Instructions.Mov, node: *DoublyLinkedList.Node, stdout: *std.Io
                     // Handle the special case when there IS a displacement
                     // when the MODE is set to "No Displacement".
                     if (mod_reg_rm.rm == 0b110) {
-                        var data_lo: u8 = undefined;
-                        var data_hi: u16 = undefined;
+                        const disp_lo_byte = try getNextByte(&current);
+                        const disp_lo = disp_lo_byte.data;
+                        count += 1;
 
-                        if (current.next) |next| {
-                            const lo_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                            data_lo = lo_byte.data;
-                            count += 1;
-                            current = next;
-                        } else {
-                            return error.ExpectedDataLo;
-                        }
+                        const disp_hi_byte = try getNextByte(&current);
+                        const disp_hi: u16 = disp_hi_byte.data;
+                        count += 1;
 
-                        if (current.next) |next| {
-                            const hi_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                            data_hi = hi_byte.data;
-                            count += 1;
-                            current = next;
-                        } else {
-                            return error.ExpectedDataHi;
-                        }
-
-                        const addr = (data_hi << 8) | data_lo;
-                        try stdout.print("{s}, [{d}]\n", .{ register, addr });
+                        const displacement = (disp_hi << 8) | disp_lo;
+                        try stdout.print("{s}, [{d}]\n", .{ register, displacement });
                     } else {
                         if (d_flag) {
                             try stdout.print("{s}, [", .{register});
@@ -138,27 +113,20 @@ fn parseMov(mov: Instructions.Mov, node: *DoublyLinkedList.Node, stdout: *std.Io
                     }
                 },
                 Binary.Mode.Memory8BitDisplacement => {
+                    const disp_lo_byte = try getNextByte(&current);
+                    const disp_lo = disp_lo_byte.data;
+                    count += 1;
+
                     // 8 bit displacement allows for the displacement to be
                     // signed. It does this by performing sign extension
                     // on the byte and using that as the displacement value.
-                    var data_lo: u8 = undefined;
-
-                    if (current.next) |next| {
-                        const lo_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                        data_lo = lo_byte.data;
-                        count += 1;
-                        current = next;
-                    } else {
-                        return error.ExpectedDataLo;
-                    }
-
-                    const msb_set = (0b1000_0000 & data_lo) == 0b1000_0000;
-                    const data_hi: u16 =
+                    const msb_set = (0b1000_0000 & disp_lo) == 0b1000_0000;
+                    const disp_hi: u16 =
                         if (msb_set)
                             0b1111_1111_0000_0000
                         else
                             0b0000_0000_0000_0000;
-                    const displacement: i16 = @bitCast(data_hi | data_lo);
+                    const displacement: i16 = @bitCast(disp_hi | disp_lo);
                     const op: u8 = if (displacement >= 0) '+' else '-';
                     const register = Register.make(mod_reg_rm.reg, w_flag).emit();
 
@@ -173,28 +141,15 @@ fn parseMov(mov: Instructions.Mov, node: *DoublyLinkedList.Node, stdout: *std.Io
                     }
                 },
                 Binary.Mode.Memory16BitDisplacement => {
-                    var data_lo: u8 = undefined;
-                    var data_hi: u16 = undefined;
+                    const disp_lo_byte = try getNextByte(&current);
+                    const disp_lo = disp_lo_byte.data;
+                    count += 1;
 
-                    if (current.next) |next| {
-                        const lo_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                        data_lo = lo_byte.data;
-                        count += 1;
-                        current = next;
-                    } else {
-                        return error.ExpectedDataLo;
-                    }
+                    const disp_hi_byte = try getNextByte(&current);
+                    const disp_hi: u16 = disp_hi_byte.data;
+                    count += 1;
 
-                    if (current.next) |next| {
-                        const hi_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                        data_hi = hi_byte.data;
-                        count += 1;
-                        current = next;
-                    } else {
-                        return error.ExpectedDataHi;
-                    }
-
-                    const displacement: i16 = @bitCast((data_hi << 8) | data_lo);
+                    const displacement: i16 = @bitCast((disp_hi << 8) | disp_lo);
                     const op: u8 = if (displacement >= 0) '+' else '-';
                     const register = Register.make(mod_reg_rm.reg, w_flag).emit();
 
@@ -213,40 +168,22 @@ fn parseMov(mov: Instructions.Mov, node: *DoublyLinkedList.Node, stdout: *std.Io
         .ImmToRegMem => |m| {
             const w_flag = m.w;
 
-            var mod_reg_rm: Binary.ModeRegRm = undefined;
-            if (current.next) |next| {
-                const mod_reg_rm_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                mod_reg_rm = @bitCast(mod_reg_rm_byte.data);
-                current = next;
-                count += 1;
-            } else {
-                return error.ExpectedModRegRm;
-            }
+            const mod_reg_rm_byte = try getNextByte(&current);
+            const mod_reg_rm: Binary.ModeRegRm = @bitCast(mod_reg_rm_byte.data);
+            count += 1;
 
             try stdout.print("mov ", .{});
             switch (mod_reg_rm.mode) {
                 Binary.Mode.MemoryNoDisplacement => {
                     if (w_flag) {
-                        var disp_lo: u8 = undefined;
-                        var disp_hi: u16 = undefined;
+                        const disp_lo_byte = try getNextByte(&current);
+                        const disp_lo = disp_lo_byte.data;
+                        count += 1;
 
-                        if (current.next) |next| {
-                            const disp_lo_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                            disp_lo = disp_lo_byte.data;
-                            count += 1;
-                            current = next;
-                        } else {
-                            return error.ExpectedDispLo;
-                        }
+                        const disp_hi_byte = try getNextByte(&current);
+                        const disp_hi: u16 = disp_hi_byte.data;
+                        count += 1;
 
-                        if (current.next) |next| {
-                            const disp_hi_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                            disp_hi = disp_hi_byte.data;
-                            count += 1;
-                            current = next;
-                        } else {
-                            return error.ExpectedDispHi;
-                        }
                         const displacement = (disp_hi << 8) | disp_lo;
 
                         try stdout.print("[", .{});
@@ -270,19 +207,13 @@ fn parseMov(mov: Instructions.Mov, node: *DoublyLinkedList.Node, stdout: *std.Io
                     }
                 },
                 Binary.Mode.Memory8BitDisplacement => {
+                    const disp_lo_byte = try getNextByte(&current);
+                    const disp_lo = disp_lo_byte.data;
+                    count += 1;
+
                     // 8 bit displacement allows for the displacement to be
                     // signed. It does this by performing sign extension
                     // on the byte and using that as the displacement value.
-                    var disp_lo: u8 = undefined;
-                    if (current.next) |next| {
-                        const disp_lo_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                        disp_lo = disp_lo_byte.data;
-                        count += 1;
-                        current = next;
-                    } else {
-                        return error.ExpectedDispLo;
-                    }
-
                     const msb_set = (0b1000_0000 & disp_lo) == 0b1000_0000;
                     const disp_hi: u16 =
                         if (msb_set)
@@ -327,27 +258,14 @@ fn parseMov(mov: Instructions.Mov, node: *DoublyLinkedList.Node, stdout: *std.Io
                     try stdout.print(" {c} {d}], {s} {d}\n", .{ op, @abs(displacement), size_keyword, immediate });
                 },
                 Binary.Mode.Memory16BitDisplacement => {
-                    // Read two bytes for the displacement value
-                    var disp_lo: u8 = undefined;
-                    var disp_hi: u16 = undefined;
+                    const disp_lo_byte = try getNextByte(&current);
+                    const disp_lo = disp_lo_byte.data;
+                    count += 1;
 
-                    if (current.next) |next| {
-                        const disp_lo_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                        disp_lo = disp_lo_byte.data;
-                        count += 1;
-                        current = next;
-                    } else {
-                        return error.ExpectedDispLo;
-                    }
+                    const disp_hi_byte = try getNextByte(&current);
+                    const disp_hi: u16 = disp_hi_byte.data;
+                    count += 1;
 
-                    if (current.next) |next| {
-                        const disp_hi_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                        disp_hi = disp_hi_byte.data;
-                        count += 1;
-                        current = next;
-                    } else {
-                        return error.ExpectedDispHi;
-                    }
                     const displacement = (disp_hi << 8) | disp_lo;
                     const op: u8 = if (displacement >= 0) '+' else '-';
 
@@ -396,67 +314,33 @@ fn parseMov(mov: Instructions.Mov, node: *DoublyLinkedList.Node, stdout: *std.Io
             const register_encoding = m.reg;
             const reg = Register.make(register_encoding, w_flag);
 
+            const data_lo_byte = try getNextByte(&current);
+            const data_lo = data_lo_byte.data;
+            count += 1;
+
             if (w_flag) {
-                var data_lo: u8 = undefined;
-                var data_hi: u16 = undefined;
+                const data_hi_byte = try getNextByte(&current);
+                const data_hi: u16 = data_hi_byte.data;
+                count += 1;
 
-                if (current.next) |next| {
-                    const lo_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                    data_lo = lo_byte.data;
-                    count += 1;
-                    current = next;
-                } else {
-                    return error.ExpectedDataLo;
-                }
-
-                if (current.next) |next| {
-                    const hi_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                    data_hi = hi_byte.data;
-                    count += 1;
-                    current = next;
-                } else {
-                    return error.ExpectedDataHi;
-                }
                 const immediate: u16 = (data_hi << 8) | data_lo;
                 try stdout.print("mov {s}, {d}\n", .{ reg.emit(), immediate });
             } else {
-                var data: u8 = undefined;
-
-                if (current.next) |next| {
-                    const byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                    data = byte.data;
-                    count += 1;
-                    current = next;
-                } else {
-                    return error.ExpectedDataLo;
-                }
-                try stdout.print("mov {s}, {d}\n", .{ reg.emit(), data });
+                try stdout.print("mov {s}, {d}\n", .{ reg.emit(), data_lo });
             }
         },
         .AccToMem => |m| {
             const w_flag = m.w;
 
-            var data_lo: u8 = undefined;
-            var data_hi: u16 = undefined;
+            const disp_lo_byte = try getNextByte(&current);
+            const disp_lo = disp_lo_byte.data;
+            count += 1;
 
-            if (current.next) |next| {
-                const lo_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                data_lo = lo_byte.data;
-                count += 1;
-                current = next;
-            } else {
-                return error.ExpectedDataLo;
-            }
+            const disp_hi_byte = try getNextByte(&current);
+            const disp_hi: u16 = disp_hi_byte.data;
+            count += 1;
 
-            if (current.next) |next| {
-                const hi_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                data_hi = hi_byte.data;
-                count += 1;
-                current = next;
-            } else {
-                return error.ExpectedDataHi;
-            }
-            const addr = (data_hi << 8) | data_lo;
+            const addr = (disp_hi << 8) | disp_lo;
 
             // If we're only moving 8 bits, move into AL
             const register = if (w_flag) Register.AX else Register.AL;
@@ -464,27 +348,15 @@ fn parseMov(mov: Instructions.Mov, node: *DoublyLinkedList.Node, stdout: *std.Io
         },
         .MemToAcc => |m| {
             const w_flag = m.w;
-            var data_lo: u8 = undefined;
-            var data_hi: u16 = undefined;
+            const disp_lo_byte = try getNextByte(&current);
+            const disp_lo = disp_lo_byte.data;
+            count += 1;
 
-            if (current.next) |next| {
-                const lo_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                data_lo = lo_byte.data;
-                count += 1;
-                current = next;
-            } else {
-                return error.ExpectedDataLo;
-            }
+            const disp_hi_byte = try getNextByte(&current);
+            const disp_hi: u16 = disp_hi_byte.data;
+            count += 1;
 
-            if (current.next) |next| {
-                const hi_byte: *Disassemble.Byte = @fieldParentPtr("node", next);
-                data_hi = hi_byte.data;
-                count += 1;
-                current = next;
-            } else {
-                return error.ExpectedDataHi;
-            }
-            const addr = (data_hi << 8) | data_lo;
+            const addr = (disp_hi << 8) | disp_lo;
 
             // If we're only moving 8 bits, move into AL
             const register = if (w_flag) Register.AX else Register.AL;
